5dd84ce0c7d5e821f5cb2d7543c03b001f7b676b
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-04-20 09:13:52 +0200

    feat: First/Last, FirstOrDefaultLastOrDefault, Take/Skip and OrderBy/OrderByDescending LINQ examples added

--- Program.cs
+++ Program.cs
@@ -8,24 +8,26 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
-            IList<Student> studentList = new List<Student>() { 
-                new Student() { StudentID = 1, StudentName = "John", Age = 18, StandardID = 1 } ,
-                new Student() { StudentID = 2, StudentName = "Steve",  Age = 21, StandardID = 1 } ,
-                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18, StandardID = 2 } ,
-                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, StandardID = 2 } ,
-                new Student() { StudentID = 5, StudentName = "Ron" , Age = 21 }
+            IList<Student> studentList = new List<Student>()
+            {
+                new Student() {StudentID = 1, StudentName = "John", Age = 18, StandardID = 1},
+                new Student() {StudentID = 2, StudentName = "Steve", Age = 21, StandardID = 1},
+                new Student() {StudentID = 3, StudentName = "Bill", Age = 18, StandardID = 2},
+                new Student() {StudentID = 4, StudentName = "Ram", Age = 20, StandardID = 2},
+                new Student() {StudentID = 5, StudentName = "Ron", Age = 21}
             };
 
-            IList<Standard> standardList = new List<Standard>() { 
-                new Standard(){ StandardID = 1, StandardName="Standard 1"},
-                new Standard(){ StandardID = 2, StandardName="Standard 2"},
-                new Standard(){ StandardID = 3, StandardName="Standard 3"}
+            IList<Standard> standardList = new List<Standard>()
+            {
+                new Standard() {StandardID = 1, StandardName = "Standard 1"},
+                new Standard() {StandardID = 2, StandardName = "Standard 2"},
+                new Standard() {StandardID = 3, StandardName = "Standard 3"}
             };
 
             // SELECT EXAMPLE
             var studentNames = studentList.Select(x => x.StudentName).ToList();
-            
-            Console.Write( string.Join(", ", studentNames));
+
+            Console.Write(string.Join(", ", studentNames));
             PrintLines();
 
             // FIRST/LAST EXAMPLE
@@ -37,6 +39,32 @@ namespace LinqPractice
             Console.Write(lastStudent);
             PrintLines();
 
+            // FIRST/LAST OR DEFAULT EXAMPLE
+            var firstOrDefaultStudent = studentList.FirstOrDefault();
+            Console.Write(firstOrDefaultStudent);
+            PrintLines();
+
+            var lastOrDefaultStudent = studentList.LastOrDefault();
+            Console.Write(lastOrDefaultStudent);
+            PrintLines();
+
+
+            // PAGINATE
+            // EXAMPLE JUMPING 1 AND TAKING THE NEXT 3
+            // TAKE/SKIP
+            var skipTakeStudents = studentList.Skip(1).Take(3);
+            Console.Write(string.Join(", \n", skipTakeStudents));
+            PrintLines();
+
+
+            // OrderBy/OrderByDescending
+            var orderByStudentsId = studentList.OrderBy(x => x.StudentID).ToList();
+            Console.Write(string.Join(", \n", orderByStudentsId));
+            PrintLines();
+
+            var orderByDescendingStudentsId = studentList.OrderByDescending(x => x.StudentID).ToList();
+            Console.Write(string.Join(", \n", orderByDescendingStudentsId));
+            PrintLines();
         }
 
         static void PrintLines()
